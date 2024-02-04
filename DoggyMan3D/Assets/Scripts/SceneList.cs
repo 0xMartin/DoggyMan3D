ef5506f0c09bcd1c4c6ee84dfbf4f22ec0965fe3
Martin Krčma <58054719+0xMartin@users.noreply.github.com>
2024-02-04 12:52:25 +0100

    update, class with level IDs, base for outro cinematic

---
+++ DoggyMan3D/Assets/Scripts/SceneList.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SceneList
+{
+
+    public static int MAIN_MENU = 0;
+    public static int MAIN_GAME_MANAGER = 1;
+    public static int CREDITS = 2;
+    public static int GAME_INTRO = 3;
+    public static int GAME_OUTRO = 4;
+
+}
